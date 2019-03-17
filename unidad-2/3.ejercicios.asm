f9b9a9002430ad0e3fc94717fcd483d0feff3594
Jéssica Ramírez <jessicaramsa@gmail.com>
2019-03-16 20:38:19 -0600

    3.Ejercicios

---
+++ unidad-2/3.ejercicios.asm
@@ -0,0 +1,12 @@
+*******************************************************
+Ejercicio  -> 9 - 6
+Restar sin utilizar sub, neg, sbb
+Solo se usará una vez la suma (add)
+-------------------------------------------------------
+debug
+a
+mov al,8
+mov bl,2
+not bl ;primer complemento
+inc bl ;segundo complemento
+add al,bl
